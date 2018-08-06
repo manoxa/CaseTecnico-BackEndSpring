822174eeb40e240041679e1556a888a0f6bc8859
Alexandre Marques Rodrigues <p000alexrodrigues@prservicos.com.br>
2018-08-06 17:35:13 -0300

    Atualização do arquivo README

--- README.md
+++ README.md
@@ -3,8 +3,15 @@
 Este projeto foi gerado com Java 1.8 e Spring Tool Suit Version: 3.9.3.
 
 Build
-Após fazer o clone do projeto execute o comando mvn clean install.
-As URL do sistema são:
+Após fazer o clone do projeto execute os seguintes passos: 
+
+1 - Crie um banco de dados mysql, chamado casetecnicodb.
+2 - No arquivo application.properties localizado em src/main/resources/ altere o login e senha do banco de dados.
+3 - Execute o comando mvn clean install.
+4 - Após a mensagem de build success do maven, verificar as tabelas do autor e Livro. 
+
+
+As URLs do sistema são:
 
 http://localhost:8080/autores
 http://localhost:8080/livros
--- src/test/java/br/com/development/CaseTecnicoBackEndSpringApplicationTests.java
+++ src/test/java/br/com/development/CaseTecnicoBackEndSpringApplicationTests.java
@@ -1,17 +1,67 @@
 package br.com.development;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import br.com.development.domain.Autor;
+import br.com.development.domain.Comentario;
+import br.com.development.domain.Livro;
+import br.com.development.services.AutoresService;
+import br.com.development.services.LivrosService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CaseTecnicoBackEndSpringApplicationTests {
 
+
+	@Autowired
+	private LivrosService livrosService;
+	
+	@Autowired
+	private AutoresService autoresService;
+	
 	@Test
-	public void contextLoads() {
+	public void deveSalvarUmAutor() {
+		List<Livro> livros = livrosService.listar();
 		
+		Autor autor = new Autor();
+		autor.setNome("Robert Rodriguez");
+		autor.setNacionalidade("Espano");
+		autor.setNascimento(new Date());
+		autor.setLivros(livros);
+		
+		autoresService.salvar(autor);
+		
+	}
+	
+	@Test
+	public void deveSalvarUmLivro() {
+		
+		List<Autor> autores = autoresService.listar();
+		Autor autor = null;
+
+		for(Autor autorSelecionado: autores) {
+			autor = autorSelecionado;
+		}
+		
+		List<Comentario> comentarios = new ArrayList<Comentario>();
+		Livro livro = new Livro();
+		
+		livro.setNome("Gerenciando containers com Kubernetes");
+		livro.setEditora("Marques Books");
+		livro.setPublicacao(new Date());
+		livro.setResumo("Livro voltado para profissionais de TI mais especificamente DevOps");
+		livro.setComentarios(comentarios);
+		livro.setAutor(autor);
+		
+		livrosService.salvar(livro);
 	}
 
 }
